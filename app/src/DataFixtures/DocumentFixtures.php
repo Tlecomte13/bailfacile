d25aa06b9fcf0f08fdd79bba5177322785171a3f
tlecomte <lecomteteddy@gmail.com>
2022-04-27 23:12:16 +0200

    feat(initial test)

--- app/src/DataFixtures/DocumentFixtures.php
+++ app/src/DataFixtures/DocumentFixtures.php
@@ -3,6 +3,8 @@
 namespace App\DataFixtures;
 
 use App\Entity\Document;
+use App\Entity\DocumentTypes;
+use App\Enum\DocumentTypesFormat;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
@@ -20,13 +22,83 @@ class DocumentFixtures extends Fixture implements DependentFixtureInterface
 
     public function load(ObjectManager $manager): void
     {
-        $document = new Document();
-        $document->setUser($this->getReference(UserFixtures::USER_REFERENCE))
-                 ->setDocumentType($this->getReference(DocumentTypesFixtures::RENTAL_AGREEMENT));
+        $documents = $this->availableDocument();
+        foreach ($documents as [$documentTypes, $user])
+        {
+            $document = new Document();
+            $document->setUser($this->getReference($user))
+                ->setDocumentType($this->getReference($documentTypes));
 
-        $manager->persist($document);
+            $manager->persist($document);
+        }
 
         $manager->flush();
+    }
+
+    /**
+     * @return array[]
+     */
+    private function availableDocument(): array
+    {
+        return [
+            [
+                DocumentTypesFixtures::RENTAL_AGREEMENT,
+                UserFixtures::USER_REFERENCE,
+            ],
+            [
+                DocumentTypesFixtures::RENTAL_AGREEMENT,
+                UserFixtures::USER_REFERENCE,
+            ],
+            [
+                DocumentTypesFixtures::RENT_GUARANTEE_AGREEMENT,
+                UserFixtures::USER_REFERENCE,
+            ],
+            [
+                DocumentTypesFixtures::RENT_GUARANTEE_AGREEMENT,
+                UserFixtures::USER_REFERENCE,
+            ],
+            [
+                DocumentTypesFixtures::SUB_LETTING_AGREEMENT,
+                UserFixtures::USER_REFERENCE,
+            ],
+            [
+                DocumentTypesFixtures::SUB_LETTING_AGREEMENT,
+                UserFixtures::USER_REFERENCE,
+            ],
+            [
+                DocumentTypesFixtures::RENTAL_AGREEMENT_AMENDMENT,
+                UserFixtures::USER_REFERENCE,
+            ],
+            [
+                DocumentTypesFixtures::RENTAL_AGREEMENT_AMENDMENT,
+                UserFixtures::USER_REFERENCE,
+            ],
+            [
+                DocumentTypesFixtures::RENT_RECEIPT,
+                UserFixtures::USER_REFERENCE,
+            ],
+            [
+                DocumentTypesFixtures::RENT_RECEIPT,
+                UserFixtures::USER_REFERENCE,
+            ],
+            [
+                DocumentTypesFixtures::RENT_INVOICE,
+                UserFixtures::USER_REFERENCE,
+            ],
+            [
+                DocumentTypesFixtures::RENT_INVOICE,
+                UserFixtures::USER_REFERENCE,
+            ],
+            [
+                DocumentTypesFixtures::LATE_PAYMENT_LETTER,
+                UserFixtures::USER_REFERENCE,
+            ],
+            [
+                DocumentTypesFixtures::LATE_PAYMENT_LETTER,
+                UserFixtures::USER_REFERENCE,
+            ],
+
+        ];
     }
 
 
---
+++ app/tests/document/get/DocumentGetTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Tests\document\get;
+
+use App\Tests\utils\DatabaseTestCase;
+
+class DocumentGetTest extends DatabaseTestCase
+{
+    protected $databaseTool;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function testLimitTenResults(): void
+    {
+
+        $response = static::createClient()->request('GET', '/api/documents');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertEquals(count($response->toArray()['hydra:member']), 10);
+    }
+}
